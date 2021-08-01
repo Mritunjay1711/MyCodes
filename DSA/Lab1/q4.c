1d341c148920542994b2b83629b62f51165ff06a
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2021-08-02 00:00:36 +0530

    new

--- DSA/Lab1/q4.c
+++ DSA/Lab1/q4.c
@@ -5,12 +5,10 @@
 int main()
 {
     int n, *arr, sum = 0;
-    
     printf("Enter the number of elements:");
     scanf("%d", &n);
     arr = (int*)malloc(n * sizeof(int));
 
-    
     for(int i = 0; i < n; i++)
     {
         printf("Enter the number %d: ", i+1);
@@ -23,7 +21,6 @@ int main()
         printf("%d ", arr[i]);
     }
     printf("\nSum: %d", sum);
-
     free(arr);
     return 0;
 }
