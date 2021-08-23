580a3d41d5554e9af2764d39032c6b9149138094
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2021-08-23 23:38:14 +0530

    new

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,29 +1,114 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+struct stack
+{
+  int size;
+  int top;
+  char * arr;
+};
+void push(struct stack **s,char data)
+{ 
+    if(is_full(*s))
+    printf("\nStack Overflow!!");
+    else
+    {   (*s)->top++;
+         (*s)->arr[(*s)->top] = data;
+    }
+}
+char pop(struct stack **s)
+{
+    if(is_empty(*s))
+     printf("Stack Overflow!!");
+     else
+     {
+         char val = (*s)->arr[(*s)->top];
+          (*s)->top--;
+          return val;
+     }
+}
+int is_empty(struct stack*s)
+{
+    if(s->top == -1)
+    return 1;
+    return 0;
+}
+int is_full(struct stack *s)
+{
+    if(s->top == s->size -1)
+    return 1;
+    return 0;
+}
+char stack_top(struct stack*s)
+{
+    return s->arr[s->top];
+}
+int is_operator(char a)
+{
+    if(a == '+' || a == '-' || a == '/' || a == '*')
+     return 1;
+     else
+     return 0;
+}
+int precedence(char a)
+{
+    if(a == '-' ||  a == '+')
+    return 2;
+   if(a == '*' || a == '/' )
+    return 3;
+    else 
+    return 0;
+} 
+char * infix_to_postfix(char *infix)
+{
+    struct stack *s1;
+    s1 = (struct stack*)malloc(sizeof(struct stack));
+    s1->size = 100;
+    s1->top = -1;
+    s1->arr = (char*)malloc(s1->size*sizeof(char));
+    char * postfix = (char*)malloc(strlen(infix +1)*sizeof(char));
+    int i = 0;
+    int j = 0;
+    while(infix[i] != '\0')
+    {
+        if(!is_operator(infix[i]))
+        {
+           postfix[j] = infix[i];
+           i++;
+           j++;
+        }
+        else
+        {
+            if(precedence(infix[i]) > precedence(stack_top(s1)))
+            {
+                push(&s1,infix[i]);
+            i++;
+            }
+            else 
+            {
+                postfix[j] = pop(&s1);
+                j++;
+            }
 
+        }
+    }
+             while(!is_empty(s1))
+             {   
+                 postfix[j] = pop(&s1);
+                 j++;
+             }
+            
+            postfix[j] = '\0';
+             
+             return postfix;
+
+}
 int main()
 {
-    printf("Enter the number of elements: ");
-    int n;
-    scanf("%d", &n);
-    int num[n];
-    double std = 0.0, mean, sum = 0.0;
-
-    printf("Enter n elements: \n");
-    for (int i = 0; i < n; ++i)
-    {
-        scanf("%f", &num[i]);
-        sum += num[i];
-    }
-
-    mean = (double)sum / n;
-    for (int i = 0; i < n; ++i)
-    {
-        sum += pow((num[i] - mean), 2);
-        
-    }
-    std= sqrt((sum) / n);
-    printf("\nStandard Deviation = %.2f", std);
-
-    return 0;
+    char *infix,*postfix;
+    infix= (char*)malloc(40*sizeof(char));
+    printf("\nEnter the expression ");
+    scanf("%s",infix);
+    postfix = infix_to_postfix(infix);
+    printf("\n %s",postfix);
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,34 @@
 #include<iostream>
-
 using namespace std;
 
-int main(){
-    float y = 12.3;
-    if(y == 12.3)
-        cout << "true";
+class person{
+    int age;
+    public:
+        person(int a)
+        {
+            age = a;
+        }
+        max_age(person p2);
+        void display()
+        {
+            cout << age;
+        }
+};
+
+person :: max_age(person p2)
+{
+    person temp;
+    if(age > p2.age)
+        temp.age = age;
     else
-        cout << "false";
-    return 0;
+        temp.age = p2.age;
+    
+    return temp;
+}
+
+int main(){
+    person p1(2);
+    person p2(5);
+    person p3 = p1.max_age(p2);
+    p3.display();
 }
